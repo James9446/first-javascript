82838c6a7a7bb1b03d6c97286bb8aefabb81a051
James Reynolds <jameshreynolds@gmail.com>
2017-08-27 12:50:14 -0700

    Initial commit. The first time I thought of a program and wrote it! This gives the number of characters in a sentence.

---
+++ JS/main.js
@@ -0,0 +1,9 @@
+//My first JavaScript program ever! Completely conceived on my own woth no prompt!
+
+var totalLength;
+var message1 = "There are "; 
+var message2 = " characters in this message!";
+var message3 = message1 + "  " + message2;
+totalLength = message3.length;
+message4 = message1 + totalLength + message2;
+document.write(message4);
