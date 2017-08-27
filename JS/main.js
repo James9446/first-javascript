4c53d4663da28e7653f7c70f08a59acf058e4017
James Reynolds <jameshreynolds@gmail.com>
2017-08-27 14:20:06 -0700

    Added a new feature that counts the number of characters in a message and returns the message with a character count. The first meassage counter was also altered to run in the console.

--- JS/main.js
+++ JS/main.js
@@ -6,4 +6,10 @@ var message2 = " characters in this message!";
 var message3 = message1 + "  " + message2;
 totalLength = message3.length;
 message4 = message1 + totalLength + message2;
-document.write(message4);
+console.log(message4); //changed from document.write to console.log in order to have the next "program" run alone.
+
+// Second JavaScript program design to receive a message and return that message with a charcter count.
+
+var message5 = prompt("Hey enter a message and see how many characters it has.");
+totalLength = message5.length
+document.write("The message " + '"' + message5 + '"' + " has " + totalLength + " characters in it."); 
